b9db00d739f744fed8e6f24127c88ad6073921e6
jezrelljolampong <jezrelljolampong17@gmail.com>
2024-04-23 00:40:58 -0600

    vb app

---
+++ WinFormsApp3/Form1.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtItem = New TextBox()
+        lsvIten = New ListView()
+        btnAdd = New Button()
+        btnUpdate = New Button()
+        btnDelete = New Button()
+        Label1 = New Label()
+        btnClear = New Button()
+        txtTotal = New TextBox()
+        btnClose = New Button()
+        btnSave = New Button()
+        LsvItemList = New ListView()
+        SuspendLayout()
+        ' 
+        ' txtItem
+        ' 
+        txtItem.BackColor = SystemColors.InactiveCaption
+        txtItem.Location = New Point(12, 13)
+        txtItem.Name = "txtItem"
+        txtItem.Size = New Size(180, 23)
+        txtItem.TabIndex = 0
+        txtItem.Text = "fgddyft"
+        ' 
+        ' lsvIten
+        ' 
+        lsvIten.BackColor = SystemColors.GrayText
+        lsvIten.Font = New Font("Segoe UI", 9.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        lsvIten.ForeColor = SystemColors.InfoText
+        lsvIten.Location = New Point(12, 56)
+        lsvIten.MultiSelect = False
+        lsvIten.Name = "lsvIten"
+        lsvIten.Size = New Size(180, 168)
+        lsvIten.TabIndex = 1
+        lsvIten.UseCompatibleStateImageBehavior = False
+        lsvIten.View = View.List
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.BackColor = SystemColors.Highlight
+        btnAdd.Location = New Point(198, 56)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(75, 23)
+        btnAdd.TabIndex = 2
+        btnAdd.Text = "Add"
+        btnAdd.UseVisualStyleBackColor = False
+        ' 
+        ' btnUpdate
+        ' 
+        btnUpdate.BackColor = SystemColors.GradientActiveCaption
+        btnUpdate.Location = New Point(198, 85)
+        btnUpdate.Name = "btnUpdate"
+        btnUpdate.Size = New Size(75, 23)
+        btnUpdate.TabIndex = 3
+        btnUpdate.Text = "Update"
+        btnUpdate.UseVisualStyleBackColor = False
+        ' 
+        ' btnDelete
+        ' 
+        btnDelete.BackColor = SystemColors.AppWorkspace
+        btnDelete.BackgroundImageLayout = ImageLayout.None
+        btnDelete.Location = New Point(198, 114)
+        btnDelete.Name = "btnDelete"
+        btnDelete.Size = New Size(75, 23)
+        btnDelete.TabIndex = 4
+        btnDelete.Tag = "btnDelete"
+        btnDelete.Text = "Delete"
+        btnDelete.UseVisualStyleBackColor = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 237)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(64, 15)
+        Label1.TabIndex = 5
+        Label1.Text = "Total Items"
+        ' 
+        ' btnClear
+        ' 
+        btnClear.BackColor = SystemColors.InactiveCaption
+        btnClear.Location = New Point(198, 143)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(75, 23)
+        btnClear.TabIndex = 6
+        btnClear.Tag = "btnDelete"
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = False
+        ' 
+        ' txtTotal
+        ' 
+        txtTotal.BackColor = SystemColors.ControlLight
+        txtTotal.BorderStyle = BorderStyle.FixedSingle
+        txtTotal.Location = New Point(73, 235)
+        txtTotal.Name = "txtTotal"
+        txtTotal.Size = New Size(52, 23)
+        txtTotal.TabIndex = 7
+        ' 
+        ' btnClose
+        ' 
+        btnClose.BackColor = SystemColors.MenuHighlight
+        btnClose.Location = New Point(198, 201)
+        btnClose.Name = "btnClose"
+        btnClose.Size = New Size(75, 23)
+        btnClose.TabIndex = 8
+        btnClose.Tag = "btnDelete"
+        btnClose.Text = "Exit"
+        btnClose.UseVisualStyleBackColor = False
+        ' 
+        ' btnSave
+        ' 
+        btnSave.BackColor = SystemColors.AppWorkspace
+        btnSave.Location = New Point(198, 172)
+        btnSave.Name = "btnSave"
+        btnSave.Size = New Size(75, 23)
+        btnSave.TabIndex = 10
+        btnSave.Text = "Save"
+        btnSave.UseVisualStyleBackColor = False
+        ' 
+        ' LsvItemList
+        ' 
+        LsvItemList.BackColor = SystemColors.HotTrack
+        LsvItemList.ForeColor = SystemColors.InfoText
+        LsvItemList.Location = New Point(297, 56)
+        LsvItemList.Name = "LsvItemList"
+        LsvItemList.Size = New Size(267, 168)
+        LsvItemList.TabIndex = 13
+        LsvItemList.UseCompatibleStateImageBehavior = False
+        LsvItemList.View = View.Details
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(579, 266)
+        Controls.Add(LsvItemList)
+        Controls.Add(btnSave)
+        Controls.Add(btnClose)
+        Controls.Add(txtTotal)
+        Controls.Add(btnClear)
+        Controls.Add(Label1)
+        Controls.Add(btnDelete)
+        Controls.Add(btnUpdate)
+        Controls.Add(btnAdd)
+        Controls.Add(lsvIten)
+        Controls.Add(txtItem)
+        ForeColor = SystemColors.Desktop
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Tag = " "
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtItem As TextBox
+    Friend WithEvents lsvIten As ListView
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnClear As Button
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents btnClose As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents LsvItemList As ListView
+
+End Class
---
+++ WinFormsApp3/Form1.vb
@@ -0,0 +1,140 @@
+﻿Imports System.Runtime.CompilerServices
+Imports MySql.Data.MySqlClient
+Imports System.Windows.Forms
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+
+Public Class Form1
+
+    Dim connectionString As String = "Server=localhost;Database=db_rivera_group_1;Uid=root;Pwd=capricorn1973@;"
+    Dim connection As New MySqlConnection(connectionString)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        getTotalItems()
+        LoadData()
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim newItem As New ListViewItem({txtItem.Text})
+        lsvIten.Items.Add(newItem)
+        getTotalItems()
+        txtItem.Clear()
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        If lsvIten.SelectedItems.Count > 0 Then
+            lsvIten.SelectedItems(0).SubItems(0).Text = txtItem.Text
+            getTotalItems()
+
+        Else
+            MessageBox.Show("Please select an item to edit.")
+        End If
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        If lsvIten.SelectedItems.Count > 0 Then
+            If MessageBox.Show("Are you sure you want to delete this item?", "Confirm Delete", MessageBoxButtons.YesNo) = DialogResult.Yes Then
+                lsvIten.Items.Remove(lsvIten.SelectedItems(0))
+                getTotalItems()
+            End If
+        Else
+            MessageBox.Show("Please select an item to delete.")
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lsvIten.Clear()
+        txtItem.Clear()
+        getTotalItems()
+    End Sub
+
+    Private Sub getTotalItems()
+
+        Dim itemCount As Integer = lsvIten.Items.Count
+        txtTotal.Text = itemCount.ToString()
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
+    Private Sub lsvIten_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lsvIten.SelectedIndexChanged
+        If lsvIten.SelectedItems.Count > 0 Then
+            Dim selectedItem = lsvIten.SelectedItems(0)
+
+            Dim newItem = selectedItem.SubItems(0).Text
+
+            txtItem.Text = newItem
+
+        Else
+            txtItem.Clear()
+
+        End If
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        For Each item As ListViewItem In lsvIten.Items
+            Dim newItem As New ListViewItem(item.Text)
+
+            For i As Integer = 1 To item.SubItems.Count - 1
+                newItem.SubItems.Add(item.SubItems(i).Text)
+            Next
+
+            Using connection As New MySqlConnection(connectionString)
+                Dim query As String = "INSERT INTO tbl_item (item) VALUES (@item);"
+                Dim command As New MySqlCommand(query, connection)
+                command.Parameters.AddWithValue("@item", newItem.Text)
+
+                Try
+                    connection.Open()
+                    If command.ExecuteNonQuery() > 0 Then
+
+                    End If
+                Catch ex As Exception
+                    MessageBox.Show(ex.Message)
+                End Try
+            End Using
+        Next
+
+        MessageBox.Show("Record added successfully.")
+        lsvIten.Clear()
+        LoadData()
+
+    End Sub
+
+    Public Sub LoadData()
+        LsvItemList.Clear()
+        LsvItemList.Columns.Add("ID")
+        LsvItemList.Columns.Add("Item")
+        Using connection As New MySqlConnection(connectionString)
+            Dim query As String = "SELECT * FROM tbl_item;"
+            Dim command As New MySqlCommand(query, connection)
+
+            Try
+                connection.Open()
+                Dim reader As MySqlDataReader = command.ExecuteReader()
+
+                While reader.Read()
+                    Dim lvItem As New ListViewItem({reader.GetInt32("id").ToString(), reader("item").ToString()})
+                    LsvItemList.Items.Add(lvItem)
+                End While
+
+                reader.Close()
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+        End Using
+    End Sub
+
+    Private Sub btnExport_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub txtItem_TextChanged(sender As Object, e As EventArgs) Handles txtItem.TextChanged
+
+    End Sub
+
+    Private Sub LsvItemList_SelectedIndexChanged(sender As Object, e As EventArgs) Handles LsvItemList.SelectedIndexChanged
+
+    End Sub
+End Class
